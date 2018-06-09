4bb7cded26130e5ae541be9ce5129e76165738f2
Nick <31705372+Nick1704@users.noreply.github.com>
2018-06-09 09:15:50 +0300

    Update pure.js

--- treesum/pure.js
+++ treesum/pure.js
@@ -5,27 +5,18 @@ let arr = [5, 7,
 	1, 8
 ];
 function treeSum(arr) {
-	let numbers = 0;
-	treeSum1(arr);
-	function treeSum1(arr) {
-		for (let i = 0; i < arr.length; i++) {
-			if (Array.isArray(arr[i])) {
-				treeSum1(arr[i]);
-			} else {
-				numbers += arr[i];
-			}
-		}
-	}
-	return numbers;	
+ let numbers = 0;
+ treeSum1(arr);
+ function treeSum1(arr) {
+   for (let i = 0; i < arr.length; i++) {
+     if (Array.isArray(arr[i])) {
+       treeSum1(arr[i]);
+     } else {
+       numbers += arr[i];
+     }
+   }
+ }
+ return numbers;	
 }
 
 console.log(treeSum(arr));
-
-
-
-// function fact2(n) {
-// 	if (n <= 1)
-// 	return 1;
-// 	return n*fact2(n-1);
-// }
-// console.log(fact2(5));
