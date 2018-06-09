3034a18c880cb0ccfdc931e457c722b3e144d688
Nick <31705372+Nick1704@users.noreply.github.com>
2018-06-09 09:10:26 +0300

    Add files via upload

---
+++ treesum/pure.js
@@ -0,0 +1,31 @@
+"use strict";
+let arr = [5, 7,
+	[4, [2], 8, [1, 3], 2],
+	[9, []],
+	1, 8
+];
+function treeSum(arr) {
+	let numbers = 0;
+	treeSum1(arr);
+	function treeSum1(arr) {
+		for (let i = 0; i < arr.length; i++) {
+			if (Array.isArray(arr[i])) {
+				treeSum1(arr[i]);
+			} else {
+				numbers += arr[i];
+			}
+		}
+	}
+	return numbers;	
+}
+
+console.log(treeSum(arr));
+
+
+
+// function fact2(n) {
+// 	if (n <= 1)
+// 	return 1;
+// 	return n*fact2(n-1);
+// }
+// console.log(fact2(5));
